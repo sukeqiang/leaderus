36e8353a2d70036e3536bcd423369a91fd27da7f
sukeqiang <sukeqiang@163.com>
2017-02-14 17:59:04 +0800

    modify

--- src/main/java/launcher/SpringBootApp.java
+++ src/main/java/launcher/SpringBootApp.java
@@ -11,7 +11,6 @@ import org.springframework.context.ConfigurableApplicationContext;
 public class SpringBootApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		ConfigurableApplicationContext ctx = SpringApplication.run(SpringBootApp.class, args);
 		Iterator<String> itors = ctx.getBeanFactory().getBeanNamesIterator();
 		while (itors.hasNext()) {
